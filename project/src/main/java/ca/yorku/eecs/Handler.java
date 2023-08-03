41b5ef1b0545e57afb35bcca6016c3a0efff0e56
Shabbir1065 <shabbir1065@gmail.com>
2023-08-03 11:25:14 -0400

    Updated the handle method in the Handler class. Now, when we try and send a request to our api in something like Postman with the address http://localhost:8080/api/v1/insertRequestNameHere, and provide JSON info as necessary, we should be able to actually update our databases.

--- project/src/main/java/ca/yorku/eecs/Handler.java
+++ project/src/main/java/ca/yorku/eecs/Handler.java
@@ -5,6 +5,7 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
 
 public class Handler implements HttpHandler{
 
@@ -15,10 +16,21 @@ public class Handler implements HttpHandler{
         // TODO Auto-generated method stub
 
         try {
-            if (request.getRequestMethod().equals("GET")) {
-                //handleGet(request);
-                System.out.println("get");
-            } else
+            URI uri = request.getRequestURI();
+            String path = uri.getPath();
+            if (path.contains("addActor")) {
+                nm.addActor(request);
+                System.out.println("We have added the actor");
+            }
+            else if (path.contains("addMovie")) {
+                nm.addMovie(request);
+                System.out.println("We have added the movie");
+            }
+            else if (path.contains("addRelationship")) {
+                nm.addRelationship(request);
+                System.out.println("We have added the relationship");
+            }
+            else
                 sendString(request, "Unimplemented method\n", 501);
         } catch (Exception e) {
             e.printStackTrace();
