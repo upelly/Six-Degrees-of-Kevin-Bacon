fd4cb272a0be98771503556a4823360fd90fa52f
Shabbir1065 <shabbir1065@gmail.com>
2023-08-03 21:18:19 -0400

    Basically done addActor method. It seems to be working just great. Will test it more tomorrow.

--- project/src/main/java/ca/yorku/eecs/Neo4jMovies.java
+++ project/src/main/java/ca/yorku/eecs/Neo4jMovies.java
@@ -3,8 +3,11 @@ package ca.yorku.eecs;
 import static org.neo4j.driver.v1.Values.parameters;
 
 import com.sun.net.httpserver.HttpExchange;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.neo4j.driver.v1.*;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Neo4jMovies {
@@ -18,16 +21,53 @@ public class Neo4jMovies {
         driver = GraphDatabase.driver(uriDb, AuthTokens.basic("neo4j","12345678"), config);
     }
 
-    public void addActor(HttpExchange request) {
-        //check if it already exists
-        /*
-        *
-        * */
+    public void addActor(HttpExchange request) throws IOException, JSONException {
 
-        //send error 400 status code if repeated
+        // Extract JSON data as a String from the request body
+        String requestBody = Utils.getBody(request);
 
+        // Parse the JSON data to access different parts of the information
+        JSONObject jsonObject = new JSONObject(requestBody);
 
-        //else, add the actor to db and send 200 status code
+        // Access individual fields in the JSON object
+
+        // Access individual fields in the JSON object with default values in case the key is missing
+        String actorId = jsonObject.optString("actorId", null);
+        String name = jsonObject.optString("name", null);
+        boolean hasOscar = jsonObject.optBoolean("hasOscar");
+
+        if (actorId == null || name == null){
+            String response = "Improper formatting. Actor was not added.";
+            Utils.sendString(request, response, 400);
+        }
+
+        else {
+            try (Session session = driver.session()) {
+                // Read operation: Check if the actorId already exists
+                try (Transaction tx = session.beginTransaction()) {
+                    StatementResult result = tx.run("MATCH (a:Actor {actorId: $actorId}) RETURN a",
+                            parameters("actorId", actorId));
+                    if (result.hasNext()) {
+                        String response = "The actor already exists!";
+                        Utils.sendString(request, response, 400);
+                        tx.failure(); // Rollback the transaction since the actor already exists
+                        return;
+                    }
+                }
+
+                // Write operation: Actor with the given actorId doesn't exist
+                // We can proceed with adding the new actor to the database.
+                try (Transaction tx = session.beginTransaction()) {
+                    tx.run("CREATE (a:Actor {actorId: $actorId, name: $name, hasOscar: $hasOscar})",
+                            parameters("actorId", actorId, "name", name, "hasOscar", hasOscar));
+                    tx.success(); // Commit the transaction
+                }
+
+                String response = "Successfully added actor with id: " + actorId + " and name: " + name;
+                Utils.sendString(request, response, 400);
+            }
+        }
+
     }
 
     public void addMovie(HttpExchange request) {
